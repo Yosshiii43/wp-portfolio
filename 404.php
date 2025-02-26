f46751962f071d333c1f3b796e79c0e4138baccc
Yosshiii43 <daisy440440@gmail.com>
2025-02-26 14:07:49 +0900

    404ページ修正

--- 404.php
+++ 404.php
@@ -1,49 +1,5 @@
-<!DOCTYPE html>
-<html lang="ja">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/destyle.css@1.0.15/destyle.css" >
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.css"><!--メインビジュアル内アニメーション用-->
-        <link rel="stylesheet" type="text/css" href="./js/slick-1.8.1/slick/slick.css"><!--スライダー(slick)用-->
-        <link rel="stylesheet" type="text/css" href="./js/slick-1.8.1/slick/slick-theme.css"><!--スライダー(slick)用-->
-        <link rel="stylesheet" href="./css/style.css">
-        <script>
-            (function(d) {
-              var config = {
-                kitId: 'hff0ssr',
-                scriptTimeout: 3000,
-                async: true
-              },
-              h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
-            })(document);
-          </script><!--Adobe Fontsより筑紫A丸ゴシック-->
-        <title>404 Not Found | Yoshino’s Portfolio Site</title>
-    </head>
-<body class="page">
-  
-    <header class="l-header p-header" id="header">
-        <p class="p-header__title">
-            <a href="#">
-                <img src="./img/header__title.png" alt="Yoshino's portfolio site" width="116px" height="57px">
-            </a>
-        </p>
-        <div class="p-header__menu">
-            <button class="p-hamburger js-hamburger">
-                <span>menuボタン</span>
-            </button>
-            <ul class="p-gmenu c-font--nunito200">
-                <li class="p-gmenu__item"><a href="#service">Service</a></li>
-                <li class="p-gmenu__item"><a href="#works">Works</a></li>
-                <li class="p-gmenu__item"><a href="#about">About</a></li>
-                <li class="p-gmenu__item"><a href="#flow">Flow</a></li>
-                <li class="p-gmenu__item"><a href="#contact">Contact</a></li>
-            </ul>
-        </div>
-    </header>
+<?php get_header(); ?>
+
     <main class="l-main">
         <div class="p-lowerPageHeading c-bg--gray">
             <div>
@@ -58,26 +14,9 @@
                     <p>アクセスされたページは見つかりませんでした。</p>
                     <p>ページが移動または削除されたか、URLの入力間違いの可能性があります。</p>
                 </div>
-                <p class="p-siteLink"><a href="">トップページへ戻る</a></p>
+                <p class="p-siteLink"><a href="<?php echo esc_url( home_url() ); ?>">トップページへ戻る</a></p>
             </div>
         </section><!--c-wrap-->
     </main>
-    <footer class="p-footer c-bg--yellow">
-        <div>
-            <ul class="p-footer__menu c-font--nunito200">
-                <li class="p-footer__menu__item"><a href="#service">Service</a></li>
-                <li class="p-footer__menu__item"><a href="#works">Works</a></li>
-                <li class="p-footer__menu__item"><a href="#about">About</a></li>
-                <li class="p-footer__menu__item"><a href="#flow">Flow</a></li>
-                <li class="p-footer__menu__item"><a href="#contact">Contact</a></li>
-            </ul>
-            <p class="p-footer__copyright c-textSmall c-font--nunito200">&copy;2024 ️Yoshino Sumi</p>
-        </div>
-    </footer>
-    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script><!--メインビジュアル内アニメーション用-->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/textillate/0.4.0/jquery.textillate.min.js"></script><!--メインビジュアル内アニメーション用-->
-    <script src="js/jquery.lettering.js"></script><!--メインビジュアル内アニメーション用-->
-    <script src="./js/slick-1.8.1/slick/slick.js"></script><!--スライダー(slick)用-->
-    <script type="text/javascript" src="./js/main.js"></script>
-</body>
-</html>
+
+<?php get_footer(); ?>
