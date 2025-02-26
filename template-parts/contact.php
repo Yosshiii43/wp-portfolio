1f002b466c810392a2d5a7e66c893aad1ceb9353
Yosshiii43 <daisy440440@gmail.com>
2025-02-26 16:50:44 +0900

    お問い合わせフォーム修正

--- template-parts/contact.php
+++ template-parts/contact.php
@@ -10,37 +10,7 @@
               <p>　3営業日以内に返信させていただきます。</p>
               <p>　万が一、返信が届かない場合は、お手数ですが迷惑メールフォルダもご確認くださいますよう、お願い申し上げます。</p>
           </div>
-          <!--
-          <form class="p-contact_form p-contactForm">
-              <div class="p-contactForm__item p-contactForm__item--required">
-                  <label for="name">お名前</label>
-                  <input type="text" name="name" placeholder="山田　太郎" required tabindex="1">
-              </div>
-              <div class="p-contactForm__item">
-                  <label for="furigana">ふりがな</label>
-                  <input type="text" name="furigana" placeholder="やまだ　たろう"  tabindex="2">
-              </div>
-              <div class="p-contactForm__item p-contactForm__item--required">
-                  <label for="company-name">会社名・担当部署など</label>
-                  <input type="text" name="company-name" placeholder="◯◯株式会社　広報部" required tabindex="3">
-              </div>
-              <div class="p-contactForm__item p-contactForm__item--required">
-                  <label for="email">メールアドレス</label>
-                  <input type="text" name="email" placeholder="sample@gmail.com" required tabindex="4">
-              </div>
-              <div class="p-contactForm__item p-contactForm__item--required">
-                  <label for="details">お問い合わせ内容</label>
-                  <textarea name="details" rows="4" required tabindex="5"></textarea>
-              </div>
-              <div class="p-contactForm__privacyPolicy">
-                  <input name="privacy" type="checkbox" required>
-                  <label for="privacy"><a href="">プライバシーポリシーに同意する</a></label>
-              </div>
-              <div class="p-contactForm__button">
-                  <input name="submit" type="submit" value="確認画面">
-              </div>
-          </form>
-          -->
+
           <?php echo do_shortcode('[contact-form-7 id="4dde3df" title="お問い合わせフォーム"]'); ?>
 
       </div>
