359c986c3689333e96d3e502100ffb9ebca8a701
Yosshiii43 <daisy440440@gmail.com>
2025-02-26 17:51:15 +0900

    リファクタリング

--- template-parts/contact.php
+++ template-parts/contact.php
@@ -2,18 +2,18 @@
 
   <div class="c-wrap">
 
-      <h2 class="c-title--frontH2 c-font--nunito200 c-fadeIn js-fadeIn">Contact</h2>
+    <h2 class="c-title--frontH2 c-font--nunito200 c-fadeIn js-fadeIn">Contact</h2>
 
-      <div class="c-fadeIn js-fadeIn">
-          <div class="p-contact__text">
-              <p>　お仕事のご依頼、ご相談は下記のお問い合わせフォームよりご連絡ください。</p>
-              <p>　3営業日以内に返信させていただきます。</p>
-              <p>　万が一、返信が届かない場合は、お手数ですが迷惑メールフォルダもご確認くださいますよう、お願い申し上げます。</p>
-          </div>
-
-          <?php echo do_shortcode('[contact-form-7 id="4dde3df" title="お問い合わせフォーム"]'); ?>
-
+    <div class="c-fadeIn js-fadeIn">
+      <div class="p-contact__text">
+        <p>　お仕事のご依頼、ご相談は下記のお問い合わせフォームよりご連絡ください。</p>
+        <p>　3営業日以内に返信させていただきます。</p>
+        <p>　万が一、返信が届かない場合は、お手数ですが迷惑メールフォルダもご確認くださいますよう、お願い申し上げます。</p>
       </div>
+
+      <?php echo do_shortcode('[contact-form-7 id="4dde3df" title="お問い合わせフォーム"]'); ?>
+
+    </div>
 
   </div><!--c-wrap-->
 
