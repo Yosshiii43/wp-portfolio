b0958325a38f2d2b04ff84a9855019d05d11457d
Yosshiii43 <daisy440440@gmail.com>
2025-10-16 05:20:11 +0900

    change:mainVisual

--- front-page.php
+++ front-page.php
@@ -1,16 +1,14 @@
 <?php get_header(); ?>
 
   <main class="l-main">
-    <div class="p-mainVisual c-bg--gray">
+    <div class="p-mainVisual">
       <div class="inner">
-        <div class="p-mainVisual__title">
-          <h1 class="p-mainVisual__title__top c-font--nunito randomAnime">Yoshino Sumi Portfolio</h1>
-          <p class="p-mainVisual__title__bottom randomAnime">Webデザイン / コーディング</p>
+        <div class="p-mainVisual__copy">
+          <h1 class="p-mainVisual__copy__top c-delay c-delay200">Webサイトを、<br class="u-none--pc u-none--tab">確かな品質で</h1>
+          <p class="p-mainVisual__copy__bottom c-delay c-delay600">デザインから<br class="u-none--pc u-none--tab">構築まで、<br class="u-none--pc u-none--tab">丁寧に仕上げます</p>
         </div>
       </div>
     </div>
-
-    <?php get_template_part( 'template-parts/concept' ); ?>
 
     <?php get_template_part( 'template-parts/service' ); ?>
 
@@ -18,9 +16,7 @@
 
     <?php get_template_part( 'template-parts/about' ); ?>
 
-    <?php
-      //get_template_part( 'template-parts/flow' ); 
-    ?>
+    <?php get_template_part( 'template-parts/flow' ); ?>
 
     <?php get_template_part( 'template-parts/contact' ); ?>
 
